b2dad8595c08b2245ff394d2e65703a332e0f210
Kaporos <theo@daron.be>
2023-12-09 16:54:02 +0100

    adding rm

---
+++ README.md
@@ -0,0 +1,22 @@
+# Usage
+
+Create one folder inside of data/ per graph you want to made. (look at samples)
+
+Then, inside graph's folder, create as many files `.txt` as you want, following this format:
+
+    labelX, labelY
+    x1, y1
+    x2, y2
+    x3, y3
+
+(etc with all your data)
+
+Then, run this to plot graphes one by one.
+
+    python main.py
+
+If you want lines leave everything by default, but you can also do
+
+    python main.py points
+
+If you want to not to draw a line between points.
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import os
+import os, sys
 
 
 for foldername in os.listdir('data'):
@@ -13,10 +13,11 @@ for foldername in os.listdir('data'):
         dataX = [[int(x.split(",")[0])] for x in content[1:] if x!=""]
         dataY = [[int(x.split(",")[1])] for x in content[1:] if x!=""]
         #Lines
-        plt.plot(dataX, dataY)
+        if len(sys.argv) > 1 and sys.argv[1] == "points":
+            plt.scatter(dataX, dataY)
+        else:
+            plt.plot(dataX, dataY)
 
-        #Points
-        #plt.scatter(dataX, dataY)
         plt.ylabel(xlabel)
         plt.xlabel(ylabel)
     plt.title(title)
