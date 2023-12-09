3805e807563f97b120cc1f010ea034845162ca39
Kaporos <theo@daron.be>
2023-12-09 16:57:28 +0100

    adding labels + better samples

--- main.py
+++ main.py
@@ -7,18 +7,20 @@ for foldername in os.listdir('data'):
     for filename in os.listdir(f):
         n = os.path.join(f, filename)
         content = open(n).read().split("\n")
-        title = foldername
-        xlabel = content[0].split(",")[0]
-        ylabel = content[0].split(",")[1]
-        dataX = [[int(x.split(",")[0])] for x in content[1:] if x!=""]
-        dataY = [[int(x.split(",")[1])] for x in content[1:] if x!=""]
+        line_label = content[0]
+        xlabel = content[1].split(",")[0]
+        ylabel = content[1].split(",")[1]
+        dataX = [[int(x.split(",")[0])] for x in content[2:] if x!=""]
+        dataY = [[int(x.split(",")[1])] for x in content[2:] if x!=""]
         #Lines
         if len(sys.argv) > 1 and sys.argv[1] == "points":
-            plt.scatter(dataX, dataY)
+            plt.scatter(dataX, dataY, label=line_label)
         else:
-            plt.plot(dataX, dataY)
+            plt.plot(dataX, dataY, label=line_label)
 
         plt.ylabel(xlabel)
         plt.xlabel(ylabel)
+    title = foldername
     plt.title(title)
+    plt.legend()
     plt.show()
