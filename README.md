9a85e459d4545b3af4dbbdf38997790344df4f86
Kaporos <theo@daron.be>
2023-12-12 09:21:06 +0100

    adding style options

--- README.md
+++ README.md
@@ -3,21 +3,35 @@
 Create one folder inside of `data` directory per graph you want to make. (look at samples)
 
 Then, inside graph's folder, create as many files `.txt` as you want (and each of them will be plotted on the same graph), following this format:
-    
+
+    plot_type
     Data label
     labelX, labelY
     x1, y1
     x2, y2
     x3, y3
 
+Plot type can be either `line`, `points` or `both` (look at samples)
+
 (etc with all your data, there already are some samples in the `data` directory)
 
-Then, run this to plot graphes one by one.
+Then, run this to plot graphes one by one with default settings, just do
 
     python main.py
 
-If you want lines leave everything by default, but you can also do
+But you can change settings by adding settings to the command this way (there is 2 of them here but you can put only one):
 
-    python main.py points
+    python main.py sett1 sett2
 
-If you want to not to draw a line between points.
+And current settings are:
+
+`nogrid` if you want to hide the background grid
+
+`dashed` if you want dashed lines instead of solid ones
+
+so
+
+    python main.py dashed
+
+is a valid command :D
+
--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
 import os, sys
 
+import matplotlib as mpl
+mpl.rcParams['figure.dpi'] = 120
+
+
+prop = fm.FontProperties(fname='Montserrat-Medium.ttf', weight='bold', size=10)
+if len([x for x in sys.argv if "clean" in x]) == 0:
+    plt.style.use([x for x in plt.style.available if "whitegrid" in x][0])
+
+dashed = len([x for x in sys.argv if "dashed" in x])
+
+#plt.style.use('https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-dark.mplstyle')
 
 for foldername in os.listdir('data'):
     f = os.path.join("data", foldername)
+    plt.figure(figsize=(10,6))
     for filename in os.listdir(f):
         n = os.path.join(f, filename)
-        content = open(n).read().split("\n")
-        line_label = content[0]
-        xlabel = content[1].split(",")[0]
-        ylabel = content[1].split(",")[1]
-        dataX = [[int(x.split(",")[0])] for x in content[2:] if x!=""]
-        dataY = [[int(x.split(",")[1])] for x in content[2:] if x!=""]
+        content = open(n).read().split("\n")        
+        type = content[0]
+        line_label = content[1]
+        xlabel = content[2].split(",")[0]
+        ylabel = content[2].split(",")[1]
+        dataX = [[int(x.split(",")[0])] for x in content[3:] if x!=""]
+        dataY = [[int(x.split(",")[1])] for x in content[3:] if x!=""]
         #Lines
-        if len(sys.argv) > 1 and sys.argv[1] == "points":
+        if type == "points":
             plt.scatter(dataX, dataY, label=line_label)
         else:
-            plt.plot(dataX, dataY, label=line_label)
+            plt.plot(dataX, dataY, ('--' if dashed else '-') + 'o' if type == "both" else '', label=line_label)
+        
 
-        plt.ylabel(xlabel)
-        plt.xlabel(ylabel)
+        plt.ylabel(xlabel, fontproperties=prop)
+        plt.xlabel(ylabel, fontproperties=prop)
     title = foldername
-    plt.title(title)
-    plt.legend()
+    plt.title(title, fontproperties=prop)
+    plt.legend(prop=prop)
     plt.show()
